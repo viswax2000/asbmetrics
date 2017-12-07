cb47fb67dc0e4c7641036793e275ad6b319271ac
viswanath.n <viswax2000@gmail.com>
2017-12-07 11:23:32 +0100

    ASB Metrics initial commit

---
+++ asbmetrics/src/main/java/com/example/asbjms/AsbmetricsApplication.java
@@ -0,0 +1,12 @@
+package com.example.asbjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsbmetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsbmetricsApplication.class, args);
+	}
+}
